9e837fba52dc286a5443fc5fa9fffaa559a0f6d4
Amlan Roy <amlanroy2500@gmail.com>
2022-10-24 02:49:13 +0530

    Added fonts and title generation
    - Added fonts
    - Added title generation methods
    - Added utils

---
+++ imageGeneration.py
@@ -0,0 +1,78 @@
+from PIL import Image, ImageDraw, ImageFont
+from utils import Post
+
+class FONTS:
+    Large = ImageFont.truetype('fonts\\IBMPlexSans-Medium.ttf', size=20)
+    SmallBold = ImageFont.truetype('fonts\\IBMPlexSans-Bold.ttf', size=12)
+    SmallMedium = ImageFont.truetype('fonts\\IBMPlexSans-Medium.ttf', size=12)
+    SmallNormal = ImageFont.truetype('fonts\\IBMPlexSans-Regular.ttf', size=12)
+    Normal = ImageFont.truetype('fonts\\IBMPlexSans-Regular.ttf', size=14)
+
+class COLORS:
+    white = (215,218,220,1)
+    gray_1 = (129,131,132,1)
+    gray_2 = (52,53,54,1)
+    gray_3 = (26,26,27,1)
+
+def text_wrap(text, font, max_width):
+    """Wrap text base on specified width. 
+    This is to enable text of width more than the image width to be display
+    nicely.
+    @params:
+        text: str
+            text to wrap
+        font: obj
+            font of the text
+        max_width: int
+            width to split the text with
+    @return
+        lines: list[str]
+            list of sub-strings
+    """
+    lines = []
+    
+    # If the text width is smaller than the image width, then no need to split
+    # just add it to the line list and return
+    if (font.getbbox(text)[2] - font.getbbox(text)[0])  <= max_width:
+        lines.append(text)
+    else:
+        #split the line by spaces to get words
+        words = text.split(' ')
+        i = 0
+        # append every word to a line while its width is shorter than the image width
+        while i < len(words):
+            line = ''
+            while i < len(words) and (font.getbbox(line + words[i])[2] - font.getbbox(line + words[i])[0]) <= max_width:
+                line = line + words[i]+ " "
+                i += 1
+            if not line:
+                line = words[i]
+                i += 1
+            lines.append(line)
+    return lines
+
+def addTextInImage(image, text,textCoordinates: tuple[int,int],font, color):
+    draw = ImageDraw.Draw(image)
+    x = textCoordinates[0]
+    y = textCoordinates[1]
+    draw.text((x, y), text , font=font, fill=color)
+    return image
+
+def generateTitle(post:Post, image:Image, opacity = 1):
+    convertedOpacity = int(255 * opacity)
+    
+    #draw top subreddit and username
+    topSubreddit = f"r/{post.subreddit}"
+    topUsername = f"u/{post.username}"
+    image = addTextInImage(image,topSubreddit,(8,8),FONTS.SmallNormal, color=COLORS.white)
+    image = addTextInImage(image,topUsername,(8,23),FONTS.SmallNormal, color=COLORS.gray_1)
+
+    return image
+
+def generateImages(post:Post,comments= []):
+    image = Image.new(mode="RGB", size=(600,800), color=COLORS.gray_3)
+    image = generateTitle(post=post, image=image)
+
+    return image
+    
+       
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from imageGeneration import generateImages
+from utils import tempPost
+
+def testingFunct():
+    print('testing function')
+    img = generateImages(post=tempPost,comments=[])
+    img.show()
 
 def main():
     print('in main')
+    testingFunct()
 
 if __name__ == "__main__":
     main()
