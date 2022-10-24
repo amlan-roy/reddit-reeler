7ef9b102f464423eca5d8d2c0d6d405856bff22c
Amlan Roy <59330872+amlan-roy@users.noreply.github.com>
2022-10-24 16:14:10 +0530

    Adding image generation (#2)
    - Added fonts
    - Added image generation methods
    - Added Post and BodyText classes.
    - Added temporary post and comments for testing

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from imageGeneration import generateImages
+from utils import tempPost,tempComments
+
+def testingFunct():
+    print('testing function')
+    img = generateImages(post=tempPost,comments=tempComments)
+    title = img[0]
+    bodyList = img[1]
+
+    title.show()
+
+    if bodyList:
+        for i in bodyList:
+            for j in i:
+                j.show()
 
 def main():
     print('in main')
+    testingFunct()
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,39 @@
+class Post:
+    def __init__(self, username, title, postText, subreddit,upvotes,commentsCount):
+        self.username = username
+        self.title = title
+        self.postText = postText
+        self.subreddit = subreddit
+        self.upvotes = upvotes
+        self.commentsCount = commentsCount
+
+class BodyText:
+    def __init__(self, username, text, upvotes) -> None:
+        super().__init__()
+        self.text = text
+        self.username = username
+        self.upvotes = upvotes
+
+tempPost = Post(
+    username='taklabhalu',
+    title='A friend has told me that they want to kill someone. A friend has told me that they want to kill someone. A friend has told me that they want to kill someone A friend has told me that they want to kill someone.',
+    postText="Im 16, and recently a friend has told me that they're planning to murder one of our classmates, a girl. At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**: I called the girl (where I live it's very late, so meeting in person wasn't an option) and told her everything. It took some convincing, but she finally said she trusted my word and would tell her parents. We plan to talk to the school and the police tomorrow. ",
+    subreddit='offmychest',
+    upvotes='3.2k',
+    commentsCount=222
+    )
+
+tempComments = [
+    BodyText(
+        username='taklabhalu_1',
+        text="Im 16, and recently a friend has told me that they're planning to murder one of our classmates, a girl. At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**: I called the girl (where I live it's very late, so meeting in person wasn't an option) and told her everything. It took some convincing, but she finally said she trusted my word and would tell her parents. We plan to talk to the school and the police tomorrow. ",
+        upvotes='111'),
+    BodyText(
+        username='taklabhalu_2',
+        text="Im 16, and recently a friend has told me that they're planning to murder one of our classmates, a girl. At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**:At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**:At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**: I called the girl (where I live it's very late, so meeting in person wasn't an option) and told her everything. It took some convincing, but she finally said she trusted my word and would tell her parents. We plan to talk to the school and the police tomorrow. ",
+        upvotes='111'),
+    BodyText(
+        username='taklabhalu_3',
+        text="Im 16, and recently a friend has told me that they're planning to murder one of our classmates, a girl. At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**: I called the girl (where I live it's very late, so meeting in person wasn't an option) and told her everything. It took some convincing, but she finally said she trusted my At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**:At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**:At first I thought they were joking, but they've talked about it more than once and I'm sure they are serious about it. This friend was in love with said girl, but got rejected and never really moved on. I don't know what to do.  \n\n\n**Update**:word and would tell her parents. We plan to talk to the school and the police tomorrow. ",
+        upvotes='111'),
+]
